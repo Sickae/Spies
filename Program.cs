77542e0629c6e8f4dc12d22992a9d8b8c48b6384
Sickae <sipos.b@outlook.com>
2018-01-14 23:48:58 +0100

    v0

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Spies
 {
@@ -9,6 +7,32 @@ namespace Spies
     {
         static void Main(string[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine());
+            solve(n);
+        }
+
+        static void solve(int n)
+        {
+            bool[,] grid = new bool[n, n];
+            List<Spy> spies = new List<Spy>();
+            spies.ForEach(c => new Spy(ref grid));
+        }
+    }
+
+    class Spy
+    {
+        int x { get; set; }
+        int y { get; set; }
+        int[] diagNb = new int[8];
+        
+        public Spy(ref bool[,] grid)
+        {
+            place(this, ref grid);
+        }
+
+        public void place(Spy spy, ref bool[,] grid)
+        {
+
         }
     }
 }
